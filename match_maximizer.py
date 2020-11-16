bfbf81970eb400ed9cc9c0a318b7944f2673a1f8
circld <circld1@gmail.com>
2020-11-15 19:01:11 -0600

    fix typing (mypy), add total_match; failing test

--- match_maximizer.py
+++ match_maximizer.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from typing import Tuple
+from typing import Callable, List, NewType, Tuple
 
-Contribution, Match = float, float
-CompanyMatch = Tuple[Tuple[Contribution, Match]]
+Contribution = NewType('Contribution', float)
+Match = NewType('Match', float)
+CompanyMatch = NewType('CompanyMatch', Tuple[Tuple[Contribution, Match]])
+
+
+def calculate_match(pp_salary: float,
+                    company_match: CompanyMatch) -> Callable[[float], float]:
+    def calc_match(amt: float) -> float:
+        match_amt = 0.
+        for contrib_pct, match_pct in company_match:
+            contrib_amt = pp_salary * contrib_pct
+            match_amt += min(amt, contrib_amt) * match_pct
+            amt -= contrib_amt
+            if amt <= 0:
+                break
+        return match_amt
+
+    return calc_match
 
 
 def display_company_strategy(*args):
-    pass
+    NotImplemented
 
 
-def maximize_match(pay_period_contribs, target_contrib, pp_limit):
-    contributions = pay_period_contribs[:]
-    for idx in range(len(contributions)):
-        if target_contrib - pp_limit > 0:
-            target_contrib -= pp_limit
-            contributions[idx] += pp_limit
-        else:
-            contributions[idx] += target_contrib
-            target_contrib = 0
-
-    return target_contrib, contributions
-
-
-def frontload_remainder(pay_period_contribs, target_contrib, pp_salary):
+def frontload_remainder(pay_period_contribs: List[float], target_contrib,
+                        pp_salary) -> List[float]:
     contributions = pay_period_contribs[:]
     for idx, pp_amt in enumerate(contributions):
         remainder = pp_salary - pp_amt
@@ -36,35 +40,58 @@ def frontload_remainder(pay_period_contribs, target_contrib, pp_salary):
     return contributions
 
 
-def per_pay_period_salary(salary: int, pay_periods: int):
+# FIXME we actually need iterate through for each match level to ensure we max
+# out the highest match % *first*
+def maximize_match(
+        pay_period_contribs: List[float], target_contrib: float,
+        pp_limit: float,
+        calc_match: Callable[[float],
+                             float]) -> Tuple[float, float, List[float]]:
+    contributions = pay_period_contribs[:]
+    total_match = 0.
+    for idx in range(len(contributions)):
+        if target_contrib - pp_limit > 0.:
+            target_contrib -= pp_limit
+            contributions[idx] += pp_limit
+            total_match += calc_match(pp_limit)
+        else:
+            contributions[idx] += target_contrib
+            total_match += calc_match(target_contrib)
+            target_contrib = 0.
+            break
+    return target_contrib, total_match, contributions
+
+
+def per_pay_period_salary(salary: int, pay_periods: int) -> float:
     return float(salary) / pay_periods
 
 
-def per_pay_period_limit(pp_salary, match: CompanyMatch):
+def per_pay_period_limit(pp_salary, match: CompanyMatch) -> float:
     limit = 0
     for contrib_pct, _ in match:
         limit += pp_salary * contrib_pct
     return limit
 
 
+def recommend_even(salary: int, pay_periods: int) -> List[float]:
+    return [float(salary) / pay_periods] * pay_periods
+
+
 def recommend_optimized(salary: int, pay_periods: int, match: CompanyMatch,
-                        target_contrib: float):
+                        target_contrib: float) -> Tuple[float, List[float]]:
     pp_salary = per_pay_period_salary(salary, pay_periods)
     pp_limit = per_pay_period_limit(pp_salary, match)
+    calc_match = calculate_match(pp_salary, match)
 
-    pay_period_contribs = [0] * pay_periods
+    pay_period_contribs = [0.] * pay_periods
 
     # first pass: max out match
-    target_contrib, pay_period_contribs = maximize_match(
-        pay_period_contribs, target_contrib, pp_limit)
+    target_contrib, total_match, pay_period_contribs = maximize_match(
+        pay_period_contribs, target_contrib, pp_limit, calc_match)
 
     # second pass: distribute the rest (frontload)
     if target_contrib > 0:
         pay_period_contribs = frontload_remainder(pay_period_contribs,
                                                   target_contrib, pp_salary)
 
-    return pay_period_contribs
-
-
-def recommend_even(salary: int, pay_periods: int):
-    return [float(salary) / pay_periods] * pay_periods
+    return total_match, pay_period_contribs
--- test_match_maximizer.py
+++ test_match_maximizer.py
@@ -1,13 +1,31 @@
 import match_maximizer as mm
 
 
-def test_maximize_match():
+def test_calc_match():
+    calc_match = mm.calculate_match(1200, ((.05, 0.5), (.05, 0.25)))
+    assert 20. == calc_match(40.)
+    assert 32.5 == calc_match(70.)
+    assert 45. == calc_match(1200.)
+
+
+def test_maximize_match_maxed():
     pp_limit = 100
     target_contrib = 5000
     pay_period_contribs = [0.] * 12
+    calc_match = mm.calculate_match(1000, ((.1, 1.), (.1, 0.5)))
     contributions = mm.maximize_match(pay_period_contribs, target_contrib,
-                                      pp_limit)
-    assert (3800, [100.] * 12) == contributions
+                                      pp_limit, calc_match)
+    assert (3800, 1200., [100.] * 12) == contributions
+
+
+def test_maximize_match_not_maxed():
+    pp_limit = 350.
+    target_contrib = 4000
+    pay_period_contribs = [0.] * 12
+    calc_match = mm.calculate_match(5000, ((.05, 1.), (.02, 0.5)))
+    contributions = mm.maximize_match(pay_period_contribs, target_contrib,
+                                      pp_limit, calc_match)
+    assert (0., 3500., [350.] * 11 + [150.]) == contributions
 
 
 def test_per_pay_period_salary():
@@ -25,11 +43,12 @@ def test_recommend_optimized():
     pay_periods = 24
     company_match = ((.03, 1.), (.02, .5))
     target_contrib = 19500
-    recommendation = mm.recommend_optimized(salary, pay_periods, company_match,
-                                            target_contrib)
+    total_match, recommendation = mm.recommend_optimized(
+        salary, pay_periods, company_match, target_contrib)
     expected = [2500.0] * 6 + [2375.] + [125.] * 17
     assert expected == recommendation
     assert target_contrib == sum(recommendation)
+    assert 0 == total_match
 
 
 def test_recommend_even():
